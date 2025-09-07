3bc710fd4f0600269011ecba3a04912c40204e88
eliu <eliuhy@163.com>
2025-09-07 16:53:08 +0800

    Add get_response function for selenium use in scrapy shell

--- scrapy_selenium_enhanced/defaults.py
+++ scrapy_selenium_enhanced/defaults.py
@@ -1,3 +1,5 @@
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.6367.91 Safari/537.36"
+
 _DEFAULT_DRIVER_ARGUMENTS = [
     '--no-sandbox',
     '--disable-dev-shm-usage',
